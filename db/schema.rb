cab720f719d5b221cbb3cd6867e6eadb9af3000c
Jo-Ohishi <ooishi_catdog@icloud.com>
2025-11-05 14:40:21 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_03_165625) do
   end
 
   create_table "comments", charset: "utf8mb3", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "prototype_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "messages", charset: "utf8mb3", force: :cascade do |t|
@@ -79,5 +84,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_03_165625) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
